79877ccf927c70486a88425ffbcd10aedb41bc4e
stanleyyork <stanleyyork@gmail.com>
2016-02-03 00:24:14 -0800

    added viewport for mobile in application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -75,7 +75,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to '/settings'
     else
       redirect_to '/signup'
     end
